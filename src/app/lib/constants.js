86dd83e2ad845edfcd9629cdb90e763cf174ae2c
Pratham <pranka0789@gmail.com>
2025-10-19 19:26:41 +0530

    updated checking on railway based integration

--- src/app/lib/constants.js
+++ src/app/lib/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/owasp-quiz';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://quizbackend-production-b34f.up.railway.app/owasp-quiz';
 export const RECRUITMENT_QUIZ_ID = "68ec712676aff2ddf3e3c0b5";
